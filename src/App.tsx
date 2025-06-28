b9048c3322e34e0b563dff945d6a87d861517034
dashiello <dashiellpepperman@gmail.com>
2025-06-28 00:52:07 -0400

    cleaned up gitignore

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import Sidebar from "@components/layout/sidebar";
-
-const App = () => {};
-
-export default App;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import Sidebar from "@/components/layout/sidebar";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
